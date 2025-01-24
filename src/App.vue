c6749ba9cc447c1f3bf50db446860812e7904cd1
florianbordignon <flobordignon@hotmail.fr>
2025-01-24 10:39:17 +0100

    feat: add supabase with exemple

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,18 @@
 </template>
 
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { supabase } from '@/config/supabaseClientConfig'
+
+const countries = ref([])
+
+async function getCountries() {
+  const { data } = await supabase.from('countries').select()
+  countries.value = data
+}
+
+onMounted(() => {
+  getCountries()
+})
 </script>
